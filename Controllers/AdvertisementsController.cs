df48b0ee8c54163eae3145bba929d550a808f919
Sagar Sutariya <64506817+Sagarsutariya08@users.noreply.github.com>
2021-04-17 02:22:02 -0400

    Update AdvertisementsController.cs

--- Controllers/AdvertisementsController.cs
+++ Controllers/AdvertisementsController.cs
@@ -105,64 +105,7 @@ namespace Assignment1.Controllers
 
             return RedirectToAction("Index", new { id = CommunityID });
         }
-
-        // For multiple files, use this
-        /*public async Task<IActionResult> Upload(ICollection<IFormFile> files)
-        {
-
-            BlobContainerClient containerClient;
         
-            // Create the container and return a container client object
-            try
-             {
-                 containerClient = await _blobServiceClient.CreateBlobContainerAsync(containerName);
-                containerClient.SetAccessPolicy(Azure.Storage.Blobs.Models.PublicAccessType.BlobContainer);
-             }
-             catch (RequestFailedException e)
-             {
-                  containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
-            }
-
-            foreach (var file in files)
-              {
-                  try{
-                     
-                  // create the blob to hold the data
-                  var blockBlob = containerClient.GetBlobClient(file.FileName);
-                     if (await blockBlob.ExistsAsync())
-                    {
-                        await blockBlob.DeleteAsync();
-                    }
-
-                     using (var memoryStream = new MemoryStream())
-                     {
-                         // copy the file data into memory
-                         await file.CopyToAsync(memoryStream);
-
-                         // navigate back to the beginning of the memory stream
-                         memoryStream.Position = 0;
-
-                         // send the file to the cloud
-                         await blockBlob.UploadAsync(memoryStream);
-                         memoryStream.Close();
-                     }
-
-                     // add the photo to the database if it uploaded successfully
-                     var image = new Advertisement();
-                     image.Url = blockBlob.Uri.AbsoluteUri;
-                     image.FileName = file.FileName;
-
-                     _context.Advertisements.Add(image);
-                     await _context.SaveChangesAsync();
-                 }
-                 catch (RequestFailedException e)
-                 {
-                    return RedirectToAction("Index");
-                 }
-             }
-            return RedirectToAction("Index");
-    }*/
-
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
